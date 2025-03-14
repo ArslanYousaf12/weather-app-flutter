e6b30f772425231a3fd7bd6589148e30ec3ed732
Arslan Yousaf <devtechofficial90@gmail.com>
2025-03-14 17:08:31 +0500

    adding dialog for city

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,6 +14,55 @@ class _HomeScreenState extends State<HomeScreen> {
   final WeatherFactory _wf = WeatherFactory(OPENWEATHER_API_KEY);
   Weather? _weather;
 
+  void _getWeather(String city) {
+    _wf.currentWeatherByCityName(city).then((weather) {
+      setState(() {
+        _weather = weather;
+      });
+    });
+  }
+
+  void _showDialog() {
+    String? city;
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Enter a city name"),
+          content: TextField(
+            onChanged: (value) {
+              city = value;
+            },
+            onSubmitted: (value) {
+              setState(() {
+                city = value;
+                _getWeather(value);
+              });
+              Navigator.pop(context);
+            },
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                if (city != null && city!.isNotEmpty) {
+                  _getWeather(city!);
+                }
+                Navigator.pop(context);
+              },
+              child: Text("Submit"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   void initState() {
     _wf.currentWeatherByCityName("London").then((weather) {
@@ -40,9 +89,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   mainAxisSize: MainAxisSize.max,
                   children: [
                     Text(
-                      "City: ${_weather!.areaName}",
+                      " ${_weather!.areaName}",
                       style: TextStyle(
-                        fontSize: 20,
+                        fontSize: 25,
                         fontWeight: FontWeight.w500,
                       ),
                     ),
@@ -70,11 +119,65 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ],
                     ),
-                    Text("Temperature: ${_weather!.temperature?.celsius}"),
-                    Text("Weather: ${_weather!.weatherMain}"),
+                    SizedBox(height: MediaQuery.sizeOf(context).height * 0.05),
+                    Column(
+                      children: [
+                        Container(
+                          width: MediaQuery.sizeOf(context).width * 0.5,
+                          height: MediaQuery.sizeOf(context).height * 0.2,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                "https://openweathermap.org/img/wn/${_weather?.weatherIcon}@4x.png",
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          _weather?.weatherDescription ?? "",
+                          style: TextStyle(fontSize: 20, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
+                    Text(
+                      "${_weather?.temperature?.celsius?.toStringAsFixed(0)}°C",
+                      style: TextStyle(fontSize: 50),
+                    ),
+                    SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Column(
+                          children: [
+                            Text("Wind", style: TextStyle(fontSize: 20)),
+                            Text(
+                              "${_weather?.windSpeed?.toStringAsFixed(1)} m/s",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: MediaQuery.sizeOf(context).width * 0.1),
+                        Column(
+                          children: [
+                            Text("Humidity", style: TextStyle(fontSize: 20)),
+                            Text(
+                              "${_weather?.humidity?.toString()}%",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showDialog,
+        child: Icon(Icons.find_in_page),
+      ),
     );
   }
 }
